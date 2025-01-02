b96147083d7c4e6e035c94c1d9b144a80b83f53b
unknown <cmonchir297@gmail.com>
2025-01-02 11:57:18 +0300

    Arracy,creating arrays,allocating elements to an array

---
+++ Simon-Chirchir/Arrays.cs
@@ -0,0 +1,35 @@
+//Defining Arrays
+using System;
+namespace NewArrays{
+    class NArrys{
+        //main method
+        public static void Main()
+        {
+            //Declare arrays 
+            //1. Integers
+            int[] IntergerArrays;
+            //2.allocating memory to arrays for 10 intergers
+            IntergerArrays=new int [10];
+            //allocating each memmory space
+            IntergerArrays[0]=10;
+            IntergerArrays[1]=10;
+            IntergerArrays[2]=10;
+            IntergerArrays[3]=10;
+            IntergerArrays[4]=10;
+            IntergerArrays[5]=10;
+            IntergerArrays[6]=10;
+            IntergerArrays[7]=10;
+            IntergerArrays[8]=10;
+            IntergerArrays[9]=10;
+            IntergerArrays[10]=10;
+
+            // Accesing elements in the  arrays using loops
+            //1.For Loop
+            Console.Write("Accesing Elements using For Loop:");
+            for (int 1 = )
+            //2.While loop
+            //3.For Each Loop
+            //4.Do-While Loop
+        }
+    }
+}
